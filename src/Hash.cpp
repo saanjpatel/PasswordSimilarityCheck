dde8640323a8a10ff87121ba9eb676284d34019e
nikkichen <nikkic1232@gmail.com>
2024-11-27 20:59:26 -0500

    updated hashmap implementation

--- src/Hash.cpp
+++ src/Hash.cpp
@@ -4,13 +4,61 @@
 
 #include "Hash.h"
 
-void HashMap::addPass(string& password) {
-    passMap[password]++;
+int HashMap::hashFunc(string &password) {
+        return password.length() % capacity;
 
+}
+
+HashMap::HashMap(int startCapacity, double limit) {
+    capacity = startCapacity;
+    numElements = 0;
+    loadFactor = limit;
+    vecNodes.resize(capacity);
+    for (int i=0; i<capacity; i++) {
+        vecNodes[i].notVacant = false;
+    }
+}
+
+void HashMap::addPass(string &password) {
+    if ((double)numElements / (double)capacity >= loadFactor) {
+        resizeVec();
+    }
+    int hashCode = hashFunc(password);
+    while (vecNodes[hashCode].notVacant == true) {   // spot is full
+        hashCode = (hashCode + 1) % capacity;
+        // break;
+    }
+    vecNodes[hashCode].password = password;
+    vecNodes[hashCode].notVacant = true;
+    numElements++;
 }
 
 bool HashMap::checkPass(string &password) {
-    return passMap.find(password) == passMap.end(); // not in the map, so it's unique
+    int hashCode = hashFunc(password);
+    while (vecNodes[hashCode].notVacant) {
+        if (vecNodes[hashCode].password == password) {
+            return true;
+        }
+        hashCode = (hashCode + 1) % capacity;
+    }
+    return false;
 }
 
+void HashMap::resizeVec() {
+    capacity *= 2; // double the capacity
+    vector<Nodes> newVec(capacity);
+    for (int i=0; i<vecNodes.size(); i++) {
+        if (vecNodes[i].notVacant) {
+            int hashCode = hashFunc(vecNodes[i].password);
+            while (newVec[hashCode].notVacant == true) {
+                hashCode = (hashCode + 1) % capacity;
+            }
+            newVec[hashCode] = vecNodes[i];
+        }
+    }
+    vecNodes = newVec;
+}
 
+int HashMap::getSize() {
+    return numElements;
+}
--- src/Hash.h
+++ src/Hash.h
@@ -2,15 +2,29 @@
 // Created by Saanj Patel on 11/22/2024.
 //
 #include <iostream>
-#include <unordered_map>
+#include <vector>
+#include <string>
 using namespace std;
 #ifndef PASSWORDSIMILARITYCHECK_HASH_H
 #define PASSWORDSIMILARITYCHECK_HASH_H
 
 class HashMap {
 private:
-    unordered_map<string, int> passMap;
+    struct Nodes {
+        string password;
+        bool notVacant;
+    };
+    vector<Nodes> vecNodes;
+    int capacity;
+    int numElements;
+    double loadFactor;
+    // hash function
+    int hashFunc(string& password);
+
 public:
+    HashMap(int startCapacity, double limit);
+    int getSize();
+    void resizeVec();
     void addPass(string& password);
     bool checkPass(string& password);
 
